3ce19169cc21de1f3cc4cac468b93f3607e14f20
Brett <brettmaster1@gmail.com>
2023-04-13 23:38:06 -0400

    packet table got fucked by git, add that

--- Assignment 4/src/ca/cosc3p91/a4/util/PacketTable.java
+++ Assignment 4/src/ca/cosc3p91/a4/util/PacketTable.java
@@ -2,6 +2,15 @@ package ca.cosc3p91.a4.util;
 
 public class PacketTable {
 
-    
+    // packetID -> byte defined in this file
+    // clientID -> long
+    // messageID -> long
+
+    // packetID, clientID (0 if connecting to server)
+    public static final byte CONNECT = 0x1;
+    // packetID, clientID
+    public static final byte DISCONNECT = 0x2;
+    // packetID, clientID, messageID
+    public static final byte ACK = 0x3;
 
 }
